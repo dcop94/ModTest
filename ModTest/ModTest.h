c86fcbb60f34923f67b8d81923f037d0b992cd1c
dcop94 <dcop94@gmail.com>
2025-01-03 11:04:09 +0900

    초기 커밋.

---
+++ ModTest/ModTest.h
@@ -0,0 +1,32 @@
+﻿
+// ModTest.h: PROJECT_NAME 애플리케이션에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'pch.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CModTestApp:
+// 이 클래스의 구현에 대해서는 ModTest.cpp을(를) 참조하세요.
+//
+
+class CModTestApp : public CWinApp
+{
+public:
+	CModTestApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CModTestApp theApp;
